2c102319ee9ee5d01560fd94d0f15c6a844a8350
Ona <onushkis@gmail.com>
2023-08-25 00:06:41 +0200

    added changes

--- produkt.js
+++ produkt.js
@@ -8,8 +8,12 @@ fetch("https://kea-alt-del.dk/t7/api/products/1525")
         console.log(product);
         document.querySelector(".produkt_info h3").textContent=
         product.productdisplayname;
+
+     
         
+
     document.querySelector(".produkt_info .brand").textContent=
         product.brandname;
         document.querySelector("img").src=`https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`
     }
+   
