8963ca097248dcadc3cc322b9c343e109c07062f
Ona <onushkis@gmail.com>
2023-08-27 22:22:57 +0200

    added changes

--- produkt.js
+++ produkt.js
@@ -10,13 +10,13 @@ fetch("https://kea-alt-del.dk/t7/api/products/1525")
         document.querySelector(".produkt_info h3").textContent=
         product.productdisplayname;
 
-        // I Dont Understand two h3 how to reach the second one, 
-
-        /* document.querySelector(".produkt_info span:nth-child(2) h4").textContent=
-        product.basecolour; */
+        // I Dont Understand two h3 how to reach the second one h4, præcis den:h4
+        document.querySelector(".produkt_info h4:nth-of-type(2) span").textContent=
+        product.basecolour; 
 
           document.querySelector(".produkt_info h4").textContent=
         product.gender; 
+       
        
         
 
