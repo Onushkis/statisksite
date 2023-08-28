3ee10d49b57880b6f15e267615545cff7b8c1989
Ona <onushkis@gmail.com>
2023-08-28 13:17:54 +0200

    added chages

--- produktlist.js
+++ produktlist.js
@@ -1,2 +1,17 @@
+fetch("https://kea-alt-del.dk/t7/api/products")
+  .then((res) => res.json())
+  .then(showProducts);
 
- 
+//Loop
+
+function showProducts(products) {
+  //Looper og kalder showProduct enkel
+  products.forEach(showProduct);
+}
+function showProduct(product) {
+    console.log(product);
+  //fange template
+  //lave en kopy
+  // ændre inhold
+  //apende
+}
